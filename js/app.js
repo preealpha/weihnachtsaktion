73d75195b68b51af5f2489a5616b0f3df61b961c
preealpha <martin.fuenning@gmail.com>
2013-12-15 11:49:22 +0100

    create app enviroment

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/**
+ * User: Martin Fünning
+ * Company: JAKOTA Design Group GmbH
+ * Date: 15.12.13
+ * Time: 11:45
+ */
+
+var App = function () {
+
+}
+App.prototype = {
+
+}
+
+var app = new App();
